0f42135fd938311958a8481ddb39105f679d10cb
Lucas Reinehr <lucas.klement@ufrgs.br>
2021-06-17 13:28:39 -0300

    Exercise 2.4

---
+++ 2_4/2_4.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Mirror an image horizontally, vertically, or both."""
+import argparse
+from PIL import Image, ImageDraw
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Paints a 10 x 10 pixels white frame into the image.")
+    parser.add_argument(
+        'in_image',
+        metavar='in',
+        help='the image to be processed')
+    parser.add_argument(
+        'out_image',
+        metavar='out',
+        help='where the processed image will be saved')
+    args = parser.parse_args()
+    
+    image = Image.open(args.in_image)
+    image = paint_frame(image)
+    image.save(args.out_image)
+
+
+def paint_frame(image: Image) -> Image:
+    """Returns a new image with a 10 x 10 pixels white frame painted on its top left corner."""
+    #Converts the image to RGB for processing.
+    original_mode = image.mode
+    new_image = image.convert("RGB")
+    
+    #Paints the frame.
+    draw = ImageDraw.Draw(new_image)
+    box = (0, 0, 9, 9)
+    color = (255, 255, 255)
+    draw.rectangle(box, fill=color)
+    
+    #Converts the image back to the original mode and returns it.
+    new_image = new_image.convert(original_mode)
+    return new_image
+
+
+if __name__ == '__main__':
+    main()
