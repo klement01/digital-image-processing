c236f965dda29a271bd8bf8b83bdb1ad573e0179
Lucas Reinehr <lucas.klement@ufrgs.br>
2021-06-17 13:42:49 -0300

    Exercise 2.5

---
+++ 2_4/2_5.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Sums and displays the pixel values of a grayscale image."""
+import argparse
+from PIL import Image, ImageDraw
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Sums and displays the pixel values of a grayscale image.')
+    parser.add_argument(
+        'in_image',
+        metavar='in',
+        help='the image to be processed')
+    args = parser.parse_args()
+    
+    image = Image.open(args.in_image)
+    result = count_values(image)
+    print(f"Result: {result}")
+
+
+def count_values(image: Image) -> int:
+    """Returns the sum of all pixel values in a grayscale image."""
+    #Converts the image to 8-bit grayscale for processing.
+    image = image.convert("L")
+    
+    #Sums the values.
+    result = sum(list(image.getdata()))
+    
+    return result
+
+
+if __name__ == '__main__':
+    main()
